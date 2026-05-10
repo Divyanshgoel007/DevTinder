916dc2a41ceabc16aa09af6a1b835ce1d856056e
Divyanshgoel007 <goeldivyansh47@gmail.com>
2026-05-10 21:43:35 +0530

    updated project

--- src/app.js
+++ src/app.js
@@ -2,17 +2,38 @@ const server = require("express")
 const app = server()
 
 
+
+app.post("/user",(req,res)=>{
+    res.send("Yes that's done")
+})
+
+app.delete("/user",(req,res)=>{    
+    res.send("Yes that's done")
+})
+
+
+app.patch(/^\/us?er$/,(req,res)=>{    // here if we will write user or uer it will work s is optional   this is used by  regx here if we add + so kitne bi s laga sakte hai 
+    res.send("Yes that's done")
+})
+
+
+app.get("/user",(req,res)=>{
+    console.log(req.query)
+    res.send("Yes that's done")
+})
+
+
+// app.get("/user/:ID/:name/:password",(req,res)=>{
+//     console.log(req.params)
+//     res.send("Yes that's done")
+// })
+
+
+//use saari http API ko access karta hai isliye hume alg alg http request send karni hoti hai
 app.use("/hello",(req,res)=>{
     res.send("hello ")
 })
 
-app.use("/namaste",(req,res)=>{
-    res.send("namaste")
-})
-
-app.use("/",(req,res)=>{
-    res.send("Hello ")
-})
 
 app.listen(2211,()=>{
     console.log("Server is running perfectly")
