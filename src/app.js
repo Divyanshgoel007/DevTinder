097f9f625022cc68085f06807eff7bb041bdc3cf
Divyanshgoel007 <goeldivyansh47@gmail.com>
2026-05-14 21:12:25 +0530

    devtinder

--- src/app.js
+++ src/app.js
@@ -5,15 +5,11 @@ const connectDB = require("./config/database")
 
 const User = require("./models/User")
 
+app.use(server.json())
 
+//signup request
 app.post("/signup",async(req,res)=>{
-    const user = new User({
-        firstName:"noni",
-        lastName:"Goel",
-        email:"goeldivyansh@gmail.com",
-        password:"divyans@123",
-        age:19
-    })
+    const user = new User(req.body)
     try{
         await user.save()
         res.send("Connected")
@@ -21,6 +17,67 @@ app.post("/signup",async(req,res)=>{
         console.log("Data not successfully saved",err)
     }
 })
+
+//finding user by there emailId
+app.get("/users",async(req,res)=>{
+    const users = req.body.email;
+    try{
+        const user = await User.find({email:users})
+        if(user.length===0){
+            res.send("cannot find user")
+        }else{
+            res.send(user)
+        }
+    }
+    catch(err){
+        res.status(400).send("Something Went Wrong")
+    }
+    
+})
+
+// finding all the users by route feed
+app.get("/feed",async(req,res)=>{
+    try{
+        const user = await User.find({})
+        if(user.length===0){
+            res.send("cannot find user")
+        }else{
+            res.send(user)
+        }
+    }
+    catch(err){
+        res.status(404).send("Something Went Wrong")
+    }    
+})
+
+//deleting a user
+app.delete("/delete",async(req,res)=>{
+    const userId=req.body.userId
+    try{
+        const user = await User.findByIdAndDelete({_id:userId})
+        res.send("user deleted successfully")
+    }
+    catch(err){
+        res.status(404).send("Something Went Wrong")
+    }     
+})
+
+//updating the user
+app.patch("/update",async(req,res)=>{
+    const userId=req.body.userId
+    const data=req.body
+    try{
+        const user = await User.findByIdAndUpdate({_id:userId},data)
+        res.send("user updated successfully")
+    }
+    catch(err){
+        res.status(404).send("Something Went Wrong")
+    }     
+})
+
+
+
+
 
 connectDB()
 .then(()=>{
